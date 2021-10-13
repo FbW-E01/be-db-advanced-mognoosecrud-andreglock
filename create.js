8b34c536ff17b397268dbc22ef982627085833e0
andreglock <andreglock@gmail.com>
2021-10-13 11:35:59 +0200

    last commit

--- create.js
+++ create.js
@@ -1,27 +1,36 @@
 import { Connect, Disconnect } from "./connection.js";
 import { User } from "./user.js"
 
-const userOne = {
-    email: "joel@example.com",
+const users = [{
+    email: "manuel@example.com",
     password: "p0potato",
     role: "User"
-}
-
-const userTwo = {
-    email: "linda@example.com",
+},
+{
+    email: "jose@example.com",
     password: "cucumber",
     role: "Admin"
-}
-
-const userThree = {
-    email: "mongo@example.com",
+},
+{
+    email: "joaquim@example.com",
     password: "password",
     role: "Editor"
-}
+}]
 
 await Connect("exampledb");
-await User.create(userOne);
+
+console.log('one');
+
+const promises = users.map(user => {
+    return User.create(user);
+})
+
+// wait for all the promises to be completed
+await Promise.all(promises)
+
+/* await User.create(userOne);
 await User.create(userTwo);
-await User.create(userThree);
+await User.create(userThree); */
 
 Disconnect();
+console.log('three');
